322e33d2807d7a42d145cf9a79eb22a2f17bc17f
Daniel Assani <devasddaniel@gmail.com>
2025-02-03 12:15:24 +0200

    update test

--- app/Domains/Alarm/Controller/router.php
+++ app/Domains/Alarm/Controller/router.php
@@ -3,8 +3,19 @@
 namespace App\Domains\Alarm\Controller;
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 Route::group(['middleware' => ['user-auth']], static function () {
+    Route::get('/test', function (Request $request) {
+        $public_ip = file_get_contents('https://api64.ipify.org?format=json');
+    $public_ip = json_decode($public_ip)->ip;
+    echo $public_ip;
+    dd($public_ip);
+    
+        Log::alert($request->all());
+        return response()->json($request->all());
+    });
     Route::get('/alarm', Index::class)->name('alarm.index');
     Route::any('/alarm/create', Create::class)->name('alarm.create');
     Route::any('/alarm/{id}', Update::class)->name('alarm.update');
